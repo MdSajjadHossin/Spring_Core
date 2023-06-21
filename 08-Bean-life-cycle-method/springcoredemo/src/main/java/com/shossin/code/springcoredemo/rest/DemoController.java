25c44495e77b75559ae76589a32557a03012148e
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2023-06-21 11:42:20 +0600

    spring bean life cycle method added

--- 08-Bean-life-cycle-method/springcoredemo/src/main/java/com/shossin/code/springcoredemo/rest/DemoController.java
+++ 08-Bean-life-cycle-method/springcoredemo/src/main/java/com/shossin/code/springcoredemo/rest/DemoController.java
@@ -10,21 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
      // define a private field for the dependency
     private Coach myCoach;
-    private Coach anotherCoach;
     @Autowired
-    public void demoController(@Qualifier("cricketCoach") Coach theCoach,
-                               @Qualifier("cricketCoach") Coach theAnotherCoach){
+    public void demoController(@Qualifier("cricketCoach") Coach theCoach){
         System.out.println("In Constructor: " + getClass().getSimpleName() );
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
     }
 
-    @GetMapping("/che ck")
-    public String check(){
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
-    }
 }
